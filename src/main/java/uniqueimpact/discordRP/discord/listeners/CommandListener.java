368b9dec10d75d73c9596dfe826dd9ed2137a755
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2023-05-13 00:29:36 +0100

    Added save on run command

--- src/main/java/uniqueimpact/discordRP/discord/listeners/CommandListener.java
+++ src/main/java/uniqueimpact/discordRP/discord/listeners/CommandListener.java
@@ -12,11 +12,15 @@ import uniqueimpact.discordRP.discord.commands.fun.ExcuseCommand;
 import uniqueimpact.discordRP.discord.commands.fun.RollCommand;
 import uniqueimpact.discordRP.discord.commands.fun.SecretCommand;
 import uniqueimpact.discordRP.discord.commands.player.*;
+import uniqueimpact.discordRP.things.Roleplay;
 
+import javax.management.relation.Role;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CommandListener implements EventListener{
+
+    Roleplay roleplay = Roleplay.getInstance();
 
     private final Map<String, Command> commands;
 
@@ -24,12 +28,14 @@ public class CommandListener implements EventListener{
 
         this.commands = new HashMap<String, Command>();
 
+        // Admin Commands
         commands.put("room", new RoomCommand());
         commands.put("character", new CharacterCommand());
         commands.put("inventory", new InventoryCommand());
         commands.put("item", new ItemCommand());
         commands.put("door", new DoorCommand());
 
+        // Player Item Commands
         commands.put("drop", new DropCommand());
         commands.put("take", new TakeCommand());
         commands.put("undress", new UndressCommand());
@@ -37,6 +43,7 @@ public class CommandListener implements EventListener{
         commands.put("undressdrop", new UndressDropCommand());
         commands.put("takewear", new TakeWearCommand());
 
+        // Fun Commands
         commands.put("roll", new RollCommand());
         commands.put("8ball", new EightBallCommand());
         commands.put("excuse", new ExcuseCommand());
@@ -54,6 +61,7 @@ public class CommandListener implements EventListener{
             String commandName = commandEvent.getName();
             if (commands.containsKey(commandName)) {
                 commandEvent.reply(commands.get(commandName).run(commandEvent)).queue();
+                roleplay.save();
             } else {
                 commandEvent.reply("That command is not implemented yet :(").queue();
             }
