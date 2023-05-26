deee15d2e5608b0992be6a052233ef7dc06bfcb5
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2023-05-26 15:00:09 +0100

    Added admin requirement to admin commands

--- src/main/java/uniqueimpact/discordRP/discord/utils/AdminChecker.java
+++ src/main/java/uniqueimpact/discordRP/discord/utils/AdminChecker.java
@@ -1,10 +1,11 @@
 package uniqueimpact.discordRP.discord.utils;
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 public class AdminChecker {
 
     public static boolean isAdmin(Member member) {
-        return true;
+        return member.hasPermission(Permission.ADMINISTRATOR);
     }
 
 }
