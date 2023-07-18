ed0b237d89f4ad655217b7738de5aaba7db83e3a
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2023-07-18 11:45:19 +0100

    Undid help command intents

--- src/main/java/uniqueimpact/discordRP/discord/commands/admin/AdminHelpCommand.java
+++ src/main/java/uniqueimpact/discordRP/discord/commands/admin/AdminHelpCommand.java
@@ -14,46 +14,46 @@ public class AdminHelpCommand implements Command {
         }
 
         return """
-                > **Room Commands**
-                > `/room create` Create a room
-                > `/room list` List all of the rooms
-                > `/room look` Look at a room
-                > `/room edit` Edit a room
-                > `/room delete` Delete a room
-                > 
-                > **Character Commands**
-                > `/character create` Create a character
-                > `/character list` List all of the characters, or all characters in a room
-                > `/character look` Look at a character
-                > `/character edit` Edit a character
-                > `/character move` Move a character
-                > `/character move-all` Move all characters (Excludes NPC characters)
-                > `/character freeze-all` Freeze all characters (Excludes NPC characters)
-                > `/character unfreeze-all` Unfreeze all characters (Excludes NPC characters)
-                > `/character delete` Delete a character
-                > 
-                > **Door Commands**
-                > `/door create` Create a door
-                > `/door list` List all of the doors
-                > `/door look` Look at a door
-                > `/door edit` Edit a door
-                > `/door delete` Delete a door
-                > 
-                > **Inventory Commands**
-                > `/inventory storage` Select the roleplay's storage inventory
-                > `/inventory room` Select a room's inventory
-                > `/inventory items` Select a character's inventory
-                > `/inventory clothes` Select a character's clothes
-                > 
-                > **Item Commands**
-                > `/item create` Create an item in the currently selected inventory
-                > `/item list` List all of the items in the currently selected inventory
-                > `/item look` Look at an item in the currently selected inventory
-                > `/item edit` Edit an item in the currently selected inventory
-                > `/item delete` Delete an item from the currently selected inventory
-                > `/item cut` Cut an item to the clipboard from the currently selected inventory
-                > `/item copy` Copy an item to the clipboard from the currently selected inventory
-                > `/item paste` Paste the item from the clipboard into the currently selected inventory
+                **Room Commands**
+                `/room create` Create a room
+                `/room list` List all of the rooms
+                `/room look` Look at a room
+                `/room edit` Edit a room
+                `/room delete` Delete a room
+                
+                **Character Commands**
+                `/character create` Create a character
+                `/character list` List all of the characters, or all characters in a room
+                `/character look` Look at a character
+                `/character edit` Edit a character
+                `/character move` Move a character
+                `/character move-all` Move all characters (Excludes NPC characters)
+                `/character freeze-all` Freeze all characters (Excludes NPC characters)
+                `/character unfreeze-all` Unfreeze all characters (Excludes NPC characters)
+                `/character delete` Delete a character
+                
+                **Door Commands**
+                `/door create` Create a door
+                `/door list` List all of the doors
+                `/door look` Look at a door
+                `/door edit` Edit a door
+                `/door delete` Delete a door
+                
+                **Inventory Commands**
+                `/inventory storage` Select the roleplay's storage inventory
+                `/inventory room` Select a room's inventory
+                `/inventory items` Select a character's inventory
+                `/inventory clothes` Select a character's clothes
+                
+                **Item Commands**
+                `/item create` Create an item in the currently selected inventory
+                `/item list` List all of the items in the currently selected inventory
+                `/item look` Look at an item in the currently selected inventory
+                `/item edit` Edit an item in the currently selected inventory
+                `/item delete` Delete an item from the currently selected inventory
+                `/item cut` Cut an item to the clipboard from the currently selected inventory
+                `/item copy` Copy an item to the clipboard from the currently selected inventory
+                `/item paste` Paste the item from the clipboard into the currently selected inventory
                 """;
 
     }
